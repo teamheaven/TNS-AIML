3a4fce8176e3a3b9b14eaad0ae5a2e6ccaf3bdad
Devesh Naik <98061080+teamheaven@users.noreply.github.com>
2025-07-29 22:57:06 +0530

    Add files via upload

---
+++ tns1.py
@@ -0,0 +1,84 @@
+"""
+print("hello world!")
+
+dog = "gir"
+cat = "diku"
+print("this is my dog's name:", dog, "this is my cat's name:", cat)
+"""
+
+"""
+num = 12
+num2 = 1.2
+num3 = "devesh"
+value = True
+division = "A"
+print(num, num2, num3, value, division)
+"""
+"""
+a = 2
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+print(a ^ b)
+"""
+"""
+a=10
+a+=10
+print(a)
+a -= 3
+print(a)
+a *= 4
+print(a)
+a /= 12
+print(a)
+a %= 12
+print(a)
+a **= 12
+print(a)
+a //= 12
+print(a)
+b=10
+print(a==b)
+"""
+
+"""
+a = 10
+b = 2
+print(a != b)
+print(a == b)
+print(a < b)
+print( a > b)
+print(a <= b)
+print( a >= b)
+"""
+
+"""
+a = 10
+b = 10
+if not(a == b & a < b):
+    print("chalo ghar chale!!")
+elif (a ==b | a<b):
+    print("kaam kr kaam!!")
+else:
+    print("Sahi hai boss!")
+"""
+
+"""
+TNS Day2:
+- Hello World
+- Data types (STRING, INT, FLOAT, BOOLEAN, CHAR)
+- Operator (Arithmetic Operator - ADD,SUB,DIV,MOD,MUL,EXPONENT) (Assignment Operator - [=,==,<,>,!=]) (Comparison Operator - ==,!=,<=.>=,*=) (Logical Operator - &, |)
+"""
+
+"""
+a = 10
+if(a % 2 == 0):
+    print("the number is even")
+else:
+    print("no it is not")
+"""
