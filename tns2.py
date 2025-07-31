235a73f1a7812bb32f7c170b87f12181c03f9c98
Devesh Naik <98061080+teamheaven@users.noreply.github.com>
2025-07-31 21:15:31 +0530

    Add files via upload

---
+++ tns2.py
@@ -0,0 +1,40 @@
+"""
+i = 0
+while (i <= 5):
+    print("Welcome back!")
+    i += 2
+
+
+for i in range (10):
+    print("fizzy!")
+    for j in range(3):
+        print("cozy!")
+        for d in range(0,5):
+            print("hello")
+            i+=1
+
+def statement():
+    i = 10
+    b = 12
+    if (i == b and i <= b):
+        print("sahi chal raha hai!")
+    elif (i != b or i >= b):
+        print("yeh bhi theek hai!")
+    else:
+        for i in range(0,10):
+            print("kuch to work kar raha hai!!")
+            i+=1
+
+statement()
+
+def lines(A,B):
+    print(A)
+    print(B)
+
+lines("hello!","dev")
+
+a = "hello"
+for char in a:
+    print(char)
+"""
+
